261f073627d27b55acc596aa5f60de9aa9b73ddd
Aurélia Hemeret <aurelia.hemeret@gmail.com>
2022-05-06 10:26:10 +0200

    search bar + design modifications

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,10 @@
 import Header from "./components/Header";
-import SearchBar from "./components/SearchBar";
 import ApiMeal from "./components/ApiMeal";
 
 function App() {
   return (
     <div>
       <Header/>
-      <SearchBar />
       <ApiMeal />
     </div>
   );
--- client/src/components/ApiMeal.js
+++ client/src/components/ApiMeal.js
@@ -4,17 +4,24 @@ import Card from "./Card";
 
 const ApiMeal = () => {
     const [data, setData] = useState([]);
+    const [searchBar, setSearchBar] = useState("");
 
     useEffect(() => {
-        axios.get("https://www.themealdb.com/api/json/v1/1/search.php?s=tomato")
+        axios
+            .get("https://www.themealdb.com/api/json/v1/1/search.php?s=" + searchBar)
             .then((res) => setData(res.data.meals));
-    }, []);
+    }, [searchBar]);
 
     return (
-        <div className="recipes_container">
-                    {data.map((meal) => (
-                        <Card key={meal.idMeal} meal={meal}/>
-                    ))}
+        <div className="body_container">
+            <div className="searchbar_container">
+                <input type="text" placeholder="Que recherchez-vous ?" onChange={(e) => setSearchBar(e.target.value)} />
+            </div>
+            <div className="recipes_container">
+                {data && data.slice(0,24).map((meal) => (
+                    <Card key={meal.idMeal} meal={meal} />
+                ))}
+            </div>
         </div>
     );
 };
